4a1c603f2fca1ff45513a1ee6546e1c3311b33c0
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2012-09-25 09:12:09 +0300

    fix composer. move autoload to tests dir.

---
+++ tests/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+$rootDir = realpath(dirname(__FILE__).'/../');
+
+require_once $rootDir.'/src/UniversalErrorCatcher/FatalErrorException.php';
+require_once $rootDir.'/src/UniversalErrorCatcher/SuppressedErrorException.php';
+require_once $rootDir.'/src/UniversalErrorCatcher/Catcher.php';
